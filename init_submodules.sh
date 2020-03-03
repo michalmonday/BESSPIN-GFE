d26fe88204641be73425cf7ea011d12992c241a8
James Clarke <jrtc27@jrtc27.com>
2020-03-03 01:42:43 +0000

    init_submodules.sh: Bring back chisel components (wanted by CI)

--- init_submodules.sh
+++ init_submodules.sh
@@ -19,5 +19,14 @@ git submodule update --init FreeRTOS-mirror busybox newlib
 git submodule update --init --recursive \
 bluespec-processors/P1/Piccolo bluespec-processors/P2/Flute \
 riscv-linux bluespec-processors/P3/Toooba riscv-tests riscv-pk
+git submodule update --init chisel_processors
+
+cd chisel_processors
+git submodule sync
+git submodule update --init rocket-chip
+git submodule update --init chipyard
+cd rocket-chip
+git submodule sync
+git submodule update --init firrtl chisel3 hardfloat
 
 cd $home
