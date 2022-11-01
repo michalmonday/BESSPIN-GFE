dce62d6dd2e9ffd8056624dc58347f673da33869
Peter Rugg <pdr32@cam.ac.uk>
2022-11-01 16:16:18 +0000

    Increase depth of utilisation reporting

--- tcl/get_ppa.tcl
+++ tcl/get_ppa.tcl
@@ -2,4 +2,4 @@ open_run impl_1
 report_timing_summary -file timing_report.log
 report_power -hierarchical_depth 5 -verbose -hier all -file power_report.log
 report_utilization -file util_report_overall.log
-report_utilization -hierarchical -hierarchical_depth 5 -file util_report_hier.log
+report_utilization -hierarchical -hierarchical_depth 20 -file util_report_hier.log
